ec21d3cb3fe99da552f9a3ca9a0cfa42810566f5
Thierry Thuon <thierry@piaf.eu>
2016-01-18 22:25:07 +0100

    Use InfluxDB as metric database

--- gateway.js
+++ gateway.js
@@ -8,6 +8,16 @@ var Event = require('./app/models/event');
 require('mongoose-double')(mongoose);
 mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
+var influx = require('influx');
+
+var client = influx({
+    // or single-host configuration
+    host : 'localhost',
+    username : 'smartdom',
+    password : 'smartdom',
+    database : 'smartdom'
+});
+
 console.log("Start gateway");
 
 var sp = new SerialPort("/dev/ttyACM0", {
@@ -24,6 +34,15 @@ sp.on('open', function(){
         }
         sensorData.payload = parseFloat(sensorData.payload);
         var event = new Event(sensorData);
+
+        var influxPoint = {
+            nodeId: sensorData.nodeId,
+            payload: sensorData.payload,
+            subType: sensorData.subType,
+            time : new Date()
+        };
+        client.writePoint(event.subType, influxPoint, null, function(err, response) {});
+
         event.save(function(err, data){});
     });
 });
