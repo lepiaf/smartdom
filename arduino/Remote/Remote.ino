ba18f28661c0d5532dadae183ce8aeb8c94206be
Thierry Thuon <thierry@piaf.eu>
2016-09-24 12:07:08 +0200

    first version remote

--- arduino/Remote/Remote.ino
+++ arduino/Remote/Remote.ino
@@ -9,30 +9,35 @@ const byte LCD_D4 = 5;
 const byte LCD_D5 = 4;
 const byte LCD_D6 = 3;
 const byte LCD_D7 = 2;
-const byte ENCODER_PIN_1 = 6;
-const byte ENCODER_PIN_2 = 7;
-const byte ENCODER_PIN_SW = 8;
+const byte ENCODER_PIN_1 = 8;
+const byte ENCODER_PIN_2 = 9;
+const byte ENCODER_PIN_SW = 6;
 
-uint8_t rotaryValue = 0;
+int rotaryValue = 0;
 long oldPositionRotary = -999;
 Encoder rotaryButton(ENCODER_PIN_1, ENCODER_PIN_2);
 
 Bounce debouncerRotaryButton = Bounce();
+
+char cursorMenuLine1 = '>';
+char cursorMenuLine2 = ' ';
+char cursorMenuLine3 = ' ';
+char cursorMenuLine4 = ' ';
 
 // init lisqui menu with lcd
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 LiquidMenu menu(lcd);
 
 // home screen
-LiquidLine home_line1(1, 0, "1.Télécommande");
-LiquidLine home_line2(1, 1, "2.Chauffage");
-LiquidLine home_line3(1, 2, "3.Autre");
-LiquidLine home_line4(1, 3, " I/O TV  | I/O son ");
+LiquidLine home_line1(0, 0, cursorMenuLine1, "1.Telecommande");
+LiquidLine home_line2(0, 1, cursorMenuLine2, "2.Chauffage");
+LiquidLine home_line3(0, 2, cursorMenuLine3, "3.Autre");
+LiquidLine home_line4(0, 3, cursorMenuLine4, " I/O TV  | I/O son ");
 LiquidScreen homeScreen(home_line1, home_line2, home_line3, home_line4);
 
 // volume screen
 LiquidLine volume_line1(1, 0, "Volume TV");
-LiquidLine volume_line2(1, 1, ">>>>>>>>>>");
+LiquidLine volume_line2(1, 1, " - << o >> +");
 LiquidLine volume_line3(1, 2, "");
 LiquidLine volume_line4(1, 3, "");
 LiquidScreen volumeScreen(volume_line1, volume_line2, volume_line3, volume_line4);
@@ -49,34 +54,104 @@ void setup()
   pinMode(ENCODER_PIN_SW, INPUT_PULLUP);
   debouncerRotaryButton.attach(ENCODER_PIN_SW);
   debouncerRotaryButton.interval(5);
-  
+
   menu.add_screen(homeScreen);
   menu.add_screen(volumeScreen);
+  menu.update();
 }
 
+int step = 0;
+char direction;
+int selectedMenuLine = 1;
 
 void loop()
 {
   debouncerRotaryButton.update();
   int rotaryButtonValue = debouncerRotaryButton.read();
   long positionRotary = rotaryButton.read();
+
   if (oldPositionRotary != positionRotary) {
+    if (positionRotary == 0) {
+      step = 0;
+    }
+
+    if (oldPositionRotary < positionRotary and (positionRotary % 4) == 0) {
+      oldPositionRotary = positionRotary;
+      step++;
+      direction = '+';
+    }
+
+    if (oldPositionRotary > positionRotary and (positionRotary % 4) == 0) {
+      oldPositionRotary = positionRotary;
+      step--;
+      direction = '-';
+    }
 
     if (currentMenu == HOME) {
-      rotaryValue = rotaryValue + positionRotary;
-      rotaryValue = constrain(rotaryValue, 1, 4);
+      if (step < 1) {
+        step = 1;
+      }
+
+      if (step > 4) {
+        step = 4;
+      }
+      switch (step) {
+        case 1:
+          cursorMenuLine1 = '>';
+          cursorMenuLine2 = ' ';
+          cursorMenuLine3 = ' ';
+          cursorMenuLine4 = ' ';
+          break;
+        case 2:
+          cursorMenuLine1 = ' ';
+          cursorMenuLine2 = '>';
+          cursorMenuLine3 = ' ';
+          cursorMenuLine4 = ' ';
+          break;
+        case 3:
+          cursorMenuLine1 = ' ';
+          cursorMenuLine2 = ' ';
+          cursorMenuLine3 = '>';
+          cursorMenuLine4 = ' ';
+          break;
+        case 4:
+          cursorMenuLine1 = ' ';
+          cursorMenuLine2 = ' ';
+          cursorMenuLine3 = ' ';
+          cursorMenuLine4 = '>';
+          break;
+        default:
+          break;
+      }
+
+      if (step != selectedMenuLine) {
+        menu.update();
+      }
+      selectedMenuLine = step;
     }
 
     if (currentMenu == VOLUME) {
-      rotaryValue = positionRotary;
+      if (step == 2) {
+        step = 1;
+        Serial.println("raise volume");
+      }
+
+      if (step == 0) {
+        step = 1;
+        Serial.println("lower volume");
+      }
     }
   }
-Serial.println(rotaryButtonValue);
-  if (currentMenu == HOME) {
+
+  if (currentMenu == HOME and rotaryButtonValue == LOW) {
     menu.change_screen(volumeScreen);
     menu.update();
     currentMenu = VOLUME;
+    delay(200);
+
+    return;
   }
 
 }
+
 
